9c7772d92cb04fdb0c8f2fcb0fb1080be9a85237
Akrom <103505768+AkromPolatov@users.noreply.github.com>
2026-06-11 11:06:31 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'aviakassa-v1';
+const FILES = ['./aviakassa.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
